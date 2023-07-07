74625d33a8a9b04661680e8cf66af7ea5562fee8
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-07-07 23:35:23 +0100

    Hero Section

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client";
 
-function CustomButton() {
+import React from 'react'
+import Image from 'next/image'
+import { CustomButtonProps } from '@/types';
+
+function CustomButton({title, containerStyles, handleClick}: CustomButtonProps) {
   return (
-    <div>CustomButton</div>
+      <button
+          disabled={false}
+          type={"button"}
+          className={`custom-btn ${containerStyles}`}
+          onClick={handleClick}
+
+      >
+          <span className={`flex-1`}>
+              {title}
+              
+              
+          </span>
+          </button>
   )
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,13 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
+import CustomButton from './CustomButton'
 
 function Hero() {
+
+    const handleScroll = () => {
+
+    }
   return (
       <div className='hero'>
           <div className="flex-1 pt-36 padding-x">
@@ -10,6 +16,24 @@ function Hero() {
               </h1>
 
               <p className='hero__subtitle'> streamline your car purchase with our smooth booking process</p>
+
+              <CustomButton
+                  title="Explore Cars"
+
+              containerStyles="text-black bg-[#00FFC8]  rounded-full mt-10"
+              handleClick={handleScroll}
+              />
+
+          </div>
+          <div className='hero__image-container'>
+              <div className="hero__image">
+                  <Image src="/heroc.png" alt='hero'
+                      fill className='object-contain '  />                   
+              </div>
+                  <div className='hero__image-overlay'>
+               <Image src="/hero-bg.png" alt='hero'
+                      fill className='object-contain relative'  />
+              </div>
           </div>
       </div>
   )
