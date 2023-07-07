a983863a19b299894796c81643698eaa9f26ee5d
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-07-07 14:50:55 +0100

    first carshop commit

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Hero() {
+  return (
+      <div className='hero'>
+          <div className="flex-1 pt-36 padding-x">
+              <h1 className="hero__title">
+                  Buy or rent a car anywhere around the world 🗺  - easily and affordable!!
+              </h1>
+
+              <p className='hero__subtitle'> streamline your car purchase with our smooth booking process</p>
+          </div>
+      </div>
+  )
+}
+
+export default Hero
