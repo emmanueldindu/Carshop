058840c3fb3f1776fb2e2eb0acbbebe33767f2f4
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-07-09 00:30:49 +0100

    worked on search props

---
+++ components/SearchManufacture.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SearchManufacturerProps } from '@/types'
+
+
+const SearchManufacture = ({ manufacturer,
+setManufacturer }: SearchManufacturerProps ) => {
+    
+  return (
+    <div>
+      search
+    </div>
+  )
+}
+
+export default SearchManufacture
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import {  Hero } from '@/components'
+import SearchBar from '@/components/SearchBar'
+import CustomFilter from '@/components/CustomFilter'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
@@ -8,7 +10,25 @@ export default function Home() {
     <main
       className="overflow-hidden"
     >
-      <Hero />
+      <Hero  />
+      <div className='mt-12 padding-x padding-y max-width ' id='discover'>
+        <div className="home__text-container">
+          <h1 className="text-4xl">
+            Our Car Exhibit
+          </h1>
+            <p>explore the vehicles and make your choice </p>
+          
+        </div>
+        <div className="home__filters flex-between">
+          <SearchBar />
+
+          <div className="home__filter-container ">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+
+          </div>
+        </div>
+      </div>
  
     </main>
   )
