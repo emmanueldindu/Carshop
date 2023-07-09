b08c82ce5292812cbcbcf7d6e47238cc61687ce9
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-07-09 14:26:11 +0100

    search filter

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,7 +4,6 @@ import SearchManufacture from './SearchManufacture'
 import { useState } from 'react'
 
 
-
 const SearchBar = () => {
     const [manufacturer, setManufacturer] = useState('')
     const handleSearch = () => {
