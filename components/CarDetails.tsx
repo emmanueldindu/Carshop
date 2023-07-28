2e1dc1a8516818cc166cbf3634f84c8040358a52
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-07-28 12:08:24 +0100

    after exam

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -18,9 +18,51 @@ const CarDetails = ({isOpen, closeModal, car} : CarDetailsProps ) => {
         
         
         >
-          <Transition.Child>
+          <Transition.Child
+           as="div"
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100'
+            leaveTo='opacity-0'
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25"></div>
             <div />
-</Transition.Child>
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+
+            <Transition.Child
+           as="div"
+            enter='ease-out duration-300'
+            enterFrom='opacity-0'
+            enterTo='opacity-100 scale-100'
+            leave='ease-in duration-200'
+            leaveFrom='opacity-100 scale-100'
+            leaveTo='opacity-0 scale-95'
+              >
+                <Dialog.Panel className='relative '>
+                  <button type='button'
+                  onClick={closeModal}
+                  >
+
+                    <Image src='/close.svg'
+                      alt='close'
+                      width={20}
+                      height={20}
+                      className='object-contain'
+
+                    />  
+
+                  </button>
+                
+                </Dialog.Panel>                
+                </Transition.Child>
+
+            </div>
+
+          </div>
         </Dialog>
               
           </Transition>
