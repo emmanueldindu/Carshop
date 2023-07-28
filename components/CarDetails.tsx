c590bccf691721a10f1fb4933fdbd5f03f78bf48
Emmanuel Dindu <emmanuelnwobodoc04@gmail.com>
2023-07-28 16:55:21 +0100

    modal

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -14,7 +14,7 @@ const CarDetails = ({isOpen, closeModal, car} : CarDetailsProps ) => {
   return (
       <>
       <Transition appear show={isOpen} as={Fragment}>
-        <Dialog as='div' className='relative z-10' onClose={closeModal}
+        <Dialog as='div' className='relative z-10 ' onClose={closeModal}
         
         
         >
@@ -27,11 +27,11 @@ const CarDetails = ({isOpen, closeModal, car} : CarDetailsProps ) => {
             leaveFrom='opacity-100'
             leaveTo='opacity-0'
           >
-            <div className="fixed inset-0 bg-black bg-opacity-25"></div>
-            <div />
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+            
           </Transition.Child>
-          <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+          <div className="fixed w-full  inset-0 overflow-y-auto">
+            <div className="flex-center  min-h-full  p-4 text-center">
 
             <Transition.Child
            as="div"
@@ -42,7 +42,7 @@ const CarDetails = ({isOpen, closeModal, car} : CarDetailsProps ) => {
             leaveFrom='opacity-100 scale-100'
             leaveTo='opacity-0 scale-95'
               >
-                <Dialog.Panel className='relative '>
+                <Dialog.Panel className='car-details__dialog-panel'>
                   <button type='button'
                   onClick={closeModal}
                   >
@@ -51,11 +51,53 @@ const CarDetails = ({isOpen, closeModal, car} : CarDetailsProps ) => {
                       alt='close'
                       width={20}
                       height={20}
-                      className='object-contain'
+                      className='object-contain relative '
 
                     />  
 
                   </button>
+                  <div className="flex-1 flex flex-col gap-3">
+
+
+                    <div className="car-details__main-image">
+                      
+                      <Image src='/hero.png' alt="car" className='object-contain' fill priority />
+                      
+                    </div>
+                    <div className="flex gap-3">
+                      <div className="flex-1 relative w-full h-24 rounded-lg">
+                      <Image src='/hero.png' alt="car" className='object-contain' fill priority />
+
+                      </div>
+
+                      <div className="flex-1 relative w-full h-24 rounded-lg">
+                      <Image src='/hero.png' alt="car" className='object-contain' fill priority />
+
+                      </div>
+
+
+                      <div className="flex-1 relative w-full h-24 rounded-lg">
+                      <Image src='/hero.png' alt="car" className='object-contain' fill priority />
+
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2 className='font-semibold capitalize '>
+                      {car.make} {car.model}
+                    </h2>
+
+                    <div className="mt-3 flex flex-wrap gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div className="flex justify-between w-full" key={key}>
+                          <h4 className='capitalize text-gray-700  font-medium '> {key.split("_").join(" ")}</h4>
+                          <p className='font-semibold'>{ value }</p>
+                        </div>
+                      ))}
+
+                    </div>
+
+                  </div>
                 
                 </Dialog.Panel>                
                 </Transition.Child>
@@ -66,7 +108,7 @@ const CarDetails = ({isOpen, closeModal, car} : CarDetailsProps ) => {
         </Dialog>
               
           </Transition>
-      details
+      
     </>
   )
 }
